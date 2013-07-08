be54755bfcc083b1a8f5c2b77fbe5a55853294cf
John Anderson <sontek@gmail.com>
2013-07-07 22:10:21 -0700

    fixed the asset override

--- notaliens/notaliens/identity/__init__.py
+++ notaliens/notaliens/identity/__init__.py
@@ -18,8 +18,8 @@ def includeme(config):
     config.include('horus')
 
     config.override_asset(
-        to_override='horus:templates'
-        , override_with='notaliens:identity/templates'
+        to_override='horus:templates/'
+        , override_with='notaliens:identity/templates/'
     )
 
     config.scan_horus('notaliens.identity.models')
