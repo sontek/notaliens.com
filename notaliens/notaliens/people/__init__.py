39c817163b4885aa27b894ea4dca4073f2893572
John Anderson <sontek@gmail.com>
2013-07-09 18:12:48 -0700

    pep8

--- notaliens/notaliens/people/__init__.py
+++ notaliens/notaliens/people/__init__.py
@@ -2,10 +2,11 @@ from pyramid.events import BeforeRender
 
 USER_INDEX = 'users'
 
+
 def includeme(config):
     config.override_asset(
-        to_override='horus:templates/edit_profile.mako'
-        , override_with='notaliens:people/templates/edit_profile.mako'
+        to_override='horus:templates/edit_profile.mako',
+        override_with='notaliens:people/templates/edit_profile.mako'
     )
 
     config.include('notaliens.people.routes')
