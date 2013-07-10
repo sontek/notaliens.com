2fd8101a13e4d8ac03b2d6d62f4435f76c0accb8
Alexandre Conrad <alexandre@surveymonkey.com>
2013-07-09 19:45:48 -0700

    add missing comma

--- notaliens/setup.py
+++ notaliens/setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
-    'colorama'
+    'colorama',
     'deform_bootstrap',
     'dogpile.cache',
     'horus',
