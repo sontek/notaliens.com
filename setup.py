2ea7723f8975b92d854d4ae8cb644912c712d110
Mario Idival <marioidival@gmail.com>
2014-03-23 02:05:34 -0300

    update entry points

--- setup.py
+++ setup.py
@@ -49,12 +49,8 @@ entry_points = """\
 main = notaliens:main
 
 [console_scripts]
+notaliens = notaliens.scripts.notaliens_cmd:main
 notaliens_create_db = notaliens.scripts.create:main
-notaliens_update_geoip = notaliens.scripts.geoip:update
-notaliens_update_geoip_csv = notaliens.scripts.geoip_csv:update
-notaliens_rebuild_index = notaliens.scripts.reindex:main
-notaliens_refresh_location = notaliens.scripts.refresh_user_location:update
-notaliens_task_queue = notaliens.tasks.worker:main
 """  # nopep8
 
 setup(
