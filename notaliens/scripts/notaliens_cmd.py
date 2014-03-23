cfe88b592a863e6b7a77f08766ef425ac47be940
Mario Idival <marioidival@gmail.com>
2014-03-23 11:08:16 -0300

    add initialize for prepare env

--- notaliens/scripts/notaliens_cmd.py
+++ notaliens/scripts/notaliens_cmd.py
@@ -1,6 +1,7 @@
 from notaliens.scripts import (
     geoip, geoip_csv, reindex, create, refresh_user_location
 )
+from notaliens.tasks import worker
 
 import argparse
 import sys
@@ -12,7 +13,8 @@ class NotAliensCommand(object):
     description = ''
     parse = argparse.ArgumentParser(prog=prog, description=description)
 
-    parse.add_argument('--init', dest='init', help='prepare your environment')
+    parse.add_argument('--init', dest='init', help='prepare your environment',
+            action='count')
     parse.add_argument('--geoip', dest='geoip', help='download geoip database',
             action='count')
     parse.add_argument('--geoip-csv', dest='geoip_csv',
@@ -41,18 +43,40 @@ class NotAliensCommand(object):
         if self.option.init:
             return self.initialize()
         elif self.option.geoip:
-            return geoip.update(self.config_file)
+            return self.geoip()
         elif self.option.geoip_csv:
-            return geoip_csv.update(self.config_file)
+            return self.geoip_csv()
         elif self.option.reindex:
-            return reindex.main(self.config_file)
+            return self.reindex()
         elif self.option.location:
-            return refresh_user_location.update(self.config_file)
+            return self.refresh_user_location()
         elif self.option.task_queue:
-            pass
+            return self.worker()
 
-   def initialize(self):
-       pass
+    def createdb(self):
+        return create.main(self.config_file)
+
+    def geoip(self):
+        return geoip.update(self.config_file)
+
+    def geoip_csv(self):
+        return geoip_csv.update(self.config_file)
+
+    def reindex(self):
+        return reindex.main(self.config_file)
+
+    def location(self):
+        return refresh_user_location.update(self.config_file)
+
+    def task_queue(self):
+        return worker.main(self.config_file)
+
+    def initialize(self):
+        """Running createdb, geoip and reindex"""
+        
+        self.createdb()
+        self.geoip()
+        self.reindex()
 
 
 def main(argv=sys.argv):
